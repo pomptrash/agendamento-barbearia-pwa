4afab67a1c36a0cef1019bbe6444608d8c211aac
João <joao.codelearning@gmail.com>
2026-08-27 23:10:59 -0300

    feat(barbearias): criação da rota POST do endpoint barbearias

--- src/app/api/barbearias/route.js
+++ src/app/api/barbearias/route.js
@@ -7,7 +7,7 @@ export async function GET(){
 
         return NextResponse.json(barbearias)
     } catch (error) {
-        console.log(error)
+        console.error(error)
         
         return NextResponse.json(
             {error: "Erro ao tentar localizar as barbearias"}, 
@@ -15,3 +15,39 @@ export async function GET(){
         )
     }
 }
+
+export async function POST(request){
+  try {
+    const body = await request.json()
+
+    const { nome, endereco, subdominio } =  body
+
+    if (!nome || !endereco || !subdominio){
+        return NextResponse.json(
+            {erro: "Preencha os campos obrigatórios"},
+            {status: 400}
+        )
+    }
+
+    const novaBarbearia = await prisma.barbearia.create({
+        data: {
+            nome: body.nome,
+            endereco: body.endereco,
+            subdominio: body.subdominio,
+            logo: body.logo ?? null
+        }
+    })
+
+    return NextResponse.json(
+        novaBarbearia, {status: 201}
+    )
+  } catch (error) {
+        console.error(error)
+
+        if (error.code === "P2002"){
+            return NextResponse.json({error: "Subdominio já utilizado"}, {status: 409})
+        }
+
+        return NextResponse.json({error: "Erro ao criar barbearia"}, {status: 500})
+    }
+} 
